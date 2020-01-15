e6585f6debb7f1945f8cb31434ef7e414533b479
unknown <mikeitt009@gmail.com>
2020-01-15 15:55:18 -0500

    added the material ui dependency and built a card form with 2 inputs and button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Formik, Field, Form } from "formik";
+import { TextField, Button, Card } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import "./App.css";
+
+const styles = makeStyles({
+  card: {
+    width: 400,
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: 50
+  }
+});
 
 function App() {
+  const classes = styles();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Formik
+        initialValues={{ firstName: "", lastName: "" }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({ values, isSubmitting }) => (
+          <>
+            <Card className={classes.card}>
+              <Form>
+                <Field
+                  type="text"
+                  name="firstName"
+                  placeholder="Enter First Name"
+                  as={TextField}
+                />
+                <div>
+                  <Field
+                    type="text"
+                    name="lastName"
+                    placeholder="Enter Last Name"
+                    as={TextField}
+                  />
+                </div>
+                <Button type="submit" disabled={isSubmitting}>
+                  Submit
+                </Button>
+                <pre>{JSON.stringify(values, null, 2)}</pre>
+              </Form>
+            </Card>
+          </>
+        )}
+      </Formik>
     </div>
   );
 }
